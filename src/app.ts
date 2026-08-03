a13a32951407650caacc81015c9c478f221fb685
Vinayak <vinayak.gupta@codexlab.in>
2026-08-03 12:54:24 +0530

    morgan implementation on every http request-logger

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import routes from "./routes/routes.js";
 import { registerSwaggerDocs } from "./swagger.js";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan('dev'));
 
 routes(app);
 registerSwaggerDocs(app);
