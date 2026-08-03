a8af71cc8425e2e74fb061bf3b731757cdc8faa5
Vinayak Gupta <vinayak.gupta@codexlab.in>
2026-08-03 21:21:10 +0530

    updated the base route for all endpoints

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,6 @@
 import contactRoutes from "./contactAssociation.route.js";
 
-const baseURL = "/v1/user/listing";
+const baseURL = "/v1/user/listing/contact-associate";
 
 const routes = (app:any)=>{
     app.use(`${baseURL}`, contactRoutes);
